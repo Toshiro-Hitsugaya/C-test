b6581c93490fb53f76b1e596dddff030c0d6940d
shadowpromax2 <156414368+shadowpromax2@users.noreply.github.com>
2024-04-04 19:39:29 +0800

    Update main.cpp
    Problems:
    1.assign函数输入的sqrt与被操作的变量相同这一特殊情况
    2.delete一块Node时，程序会自动调用一次Node的默认析构函数，无需再自己显式调用析构函数
    3.作为判定状态的标志量*ref_cnt，当它每一次递减，都要判断过它是否出现变为0的边界情况（递增注定不会出现，无需考虑）

--- main.cpp
+++ main.cpp
@@ -1,34 +1,40 @@
 #include "SmartPointer.h"
 
-SmartPointer::SmartPointer(const SmartPointer &sptr)
-{
+SmartPointer::SmartPointer(const SmartPointer &sptr) {
     pointer = sptr.pointer;
     ref_cnt = sptr.ref_cnt;
-    if(pointer!= nullptr) {
+    if (pointer != nullptr) {
         (*ref_cnt)++;
     }
 }
 
 void SmartPointer::assign(const SmartPointer &sptr)
 {
-    if(pointer == nullptr&&ref_cnt== nullptr){//new
-        if(sptr.pointer!= nullptr&&sptr.ref_cnt!= nullptr) {
-            pointer = sptr.pointer;
-            ref_cnt = sptr.ref_cnt;
-            (*ref_cnt)++;
-        } else if(sptr.pointer== nullptr){
-            pointer = sptr.pointer;
-            ref_cnt = sptr.ref_cnt;
-        }
-    } else if(pointer!= nullptr&&ref_cnt!= nullptr){
-        (*ref_cnt)--;
-        if(sptr.pointer!= nullptr&&sptr.ref_cnt!= nullptr) {
-            pointer = sptr.pointer;
-            ref_cnt = sptr.ref_cnt;
-            (*ref_cnt)++;
-        } else if(sptr.pointer == nullptr){
-            pointer = sptr.pointer;
-            ref_cnt = sptr.ref_cnt;
+    if(pointer!=sptr.pointer) {
+        if (pointer == nullptr && ref_cnt == nullptr) {//new
+            if (sptr.pointer != nullptr && sptr.ref_cnt != nullptr) {
+                pointer = sptr.pointer;
+                ref_cnt = sptr.ref_cnt;
+                (*ref_cnt)++;
+            } else if (sptr.pointer == nullptr) {
+                pointer = sptr.pointer;
+                ref_cnt = sptr.ref_cnt;
+            }
+        } else if (pointer != nullptr && ref_cnt != nullptr) {
+            (*ref_cnt)--;
+            if (*ref_cnt == 0) {
+                delete pointer;
+                pointer = nullptr;
+                ref_cnt = nullptr;
+            }
+            if (sptr.pointer != nullptr && sptr.ref_cnt != nullptr) {
+                pointer = sptr.pointer;
+                ref_cnt = sptr.ref_cnt;
+                (*ref_cnt)++;
+            } else if (sptr.pointer == nullptr) {
+                pointer = sptr.pointer;
+                ref_cnt = sptr.ref_cnt;
+            }
         }
     }
 }
@@ -38,13 +44,11 @@ SmartPointer::~SmartPointer()
     if(pointer != nullptr&&ref_cnt != nullptr) {
         (*ref_cnt)--;
         if (*ref_cnt == 0) {
-            (*pointer).~Node();
             delete pointer;
             pointer = nullptr;
             ref_cnt = nullptr;
         }
     } else if(pointer == nullptr){
-        delete pointer;
         pointer = nullptr;
         ref_cnt = nullptr;
     }
